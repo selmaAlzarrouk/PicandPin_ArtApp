166cee1f8d6ec7fdca6f7cbb4ca5404b81c2b387
selmaAlzarrouk <selma.alzarrouk@hotmail.com>
2022-06-02 18:53:47 +0100

    Tailwind css , created tailwind config js file that has the cutesy elements of css

---
+++ picandpin_frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      margin: {
+        320: '320px',
+      },
+     
