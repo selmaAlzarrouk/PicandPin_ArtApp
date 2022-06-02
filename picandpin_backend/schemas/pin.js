77d1ddd18b19c873212b9e476ac86e0c813d5f73
selmaAlzarrouk <selma.alzarrouk@hotmail.com>
2022-06-02 16:07:13 +0100

    create pin.js so established the document types and field types so name , title , type :>

---
+++ picandpin_backend/schemas/pin.js
@@ -0,0 +1,46 @@
+export default {
+    name: 'pin',
+    title: 'Pin',
+    type: 'document',
+
+    fields: [
+        {
+            name: 'title',
+            title:'Title',
+            type: 'string',
+        },
+        {
+            name: 'about',
+            title:'About',
+            type: 'string',
+        },
+        {
+            name: 'destination',
+            title:'Destination',
+            type: 'url',
+        },
+        {
+            name: 'category',
+            title:'Category',
+            type: 'string',
+        },
+        {
+            name: 'image',
+            title:'Image',
+            type: 'image',
+            options:{
+                hotspot: true  //hotspot is where you can find all the info about all the specific things that the field can have or in this case the Images :>
+            }
+        },
+        {
+            name: 'userId',
+            title:'UserID',
+            type: 'string',
+        },
+        {
+            name: 'postedBy',
+            title:'PostedBy',
+            type: 'postedBy', //type postedBy is a refernce to another document - sanity io be smart like that :>
+        },
+    ]
+}
