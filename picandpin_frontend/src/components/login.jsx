9033e473eac124d96636d78895556cf6a4e343cd
selmaAlzarrouk <selma.alzarrouk@hotmail.com>
2022-06-03 11:29:41 +0100

    updated login.jsx was giving  share video error , so I renames the mp4 file and redid the syntax properly :> , silly mistakes haha cute progress !

--- picandpin_frontend/src/components/login.jsx
+++ picandpin_frontend/src/components/login.jsx
@@ -8,7 +8,13 @@ import logo from '../assets/logowhite.png';
 
 const login = () => {
   return (
-    
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          />
+        </div>
     </div>
   )
 }
