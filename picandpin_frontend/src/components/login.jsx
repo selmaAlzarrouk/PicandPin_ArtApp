b2668f4e5a5ad81d5924184faf409ac325ad6b9c
selmaAlzarrouk <selma.alzarrouk@hotmail.com>
2022-06-03 11:28:34 +0100

    fixed asset bug , within the components  folder

--- picandpin_frontend/src/components/login.jsx
+++ picandpin_frontend/src/components/login.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import{ FcGoogle } from 'react-icons/fc';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
+
+
 const login = () => {
   return (
-    <div>
-        Login
-
+    
     </div>
   )
 }
