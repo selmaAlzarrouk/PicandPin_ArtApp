9aa09397217694674eee2ffe7377a6be79d306a9
selmaAlzarrouk <selma.alzarrouk@hotmail.com>
2022-06-04 14:34:39 +0100

    note to self revamp logo

--- picandpin_frontend/src/components/login.jsx
+++ picandpin_frontend/src/components/login.jsx
@@ -3,7 +3,7 @@ import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import{ FcGoogle } from 'react-icons/fc';
 import shareVideo from '../assets/share.mp4';
-import logo from '../assets/logowhite.png';
+import logo from '../assets/picandpinLogo.png';
 
 
 const login = () => {
@@ -22,13 +22,32 @@ const login = () => {
 
           <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0  bg-blackOverlay">
             <div className="p-5">
-              <img src={logo}/>
+              <img src={logo} width="130px" alt="logo"/>
+              </div>
+
+              <div className="shadow-2xl">
+                <GoogleLogin
+                clientId=''               
+                render= {(renderProps) => (
+                  <button
+                    type="button"
+                    className="bg-mainColor flex justify-center items-center p-3 rounded-lg "
+                       >
+                      <FcGoogle className="mr-4"/> Sign in with Google 
+
+
+                  </button>
+
+                )}
+                />
+
+                 
 
             </div>
           </div>
         </div>
     </div>
-  )
+  ) 
 }
 
 export default login
