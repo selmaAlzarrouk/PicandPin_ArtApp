0784aeb8804d22563638ad6b2954b564ceefa751
selmaAlzarrouk <selma.alzarrouk@hotmail.com>
2022-06-04 16:21:41 +0100

    react app google api token , created .env file :>

--- picandpin_frontend/src/components/login.jsx
+++ picandpin_frontend/src/components/login.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import{ FcGoogle } from 'react-icons/fc';
 import shareVideo from '../assets/share.mp4';
 import logo from '../assets/picandpinLogo.png';
 
 
 const login = () => {
+
+  const responseGoogle= (response) => {
+        console.log(response);
+
+  }
+
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       <div className="relative w-full h-full">
@@ -27,18 +34,24 @@ const login = () => {
 
               <div className="shadow-2xl">
                 <GoogleLogin
-                clientId=''               
+                clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}  //this will populate the clientId    process.env.REACT_APP_GOOGLE_API_TOKEN      
                 render= {(renderProps) => (
                   <button
                     type="button"
-                    className="bg-mainColor flex justify-center items-center p-3 rounded-lg "
+                    className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none "
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}
                        >
+                        
                       <FcGoogle className="mr-4"/> Sign in with Google 
 
 
                   </button>
 
                 )}
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy="single_host_origin"
                 />
 
                  
